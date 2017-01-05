7ec48dc0ec45489fd3b8724e3cc6c001fbaa1e32
Ugo <ugo.rayer@laposte.net>
2017-01-05 10:28:41 +0100

    Fin MI et filesystem

--- src/main.cpp
+++ src/main.cpp
@@ -62,9 +62,9 @@ int main(int argc, char **argv)
     unsigned int idIsland = 1;
     std::string nameIsland = "Node";
     float* tabMig = NULL;
-    unsigned stepM = 10;
+    unsigned stepM = 50;
     
-    unsigned nbMig = 2;
+    unsigned nbMig = 3;
     unsigned int migPart = 5;
     
     
